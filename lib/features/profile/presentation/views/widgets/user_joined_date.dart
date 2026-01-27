0708b6554e72f60537b8f17be8015f08ead953c4
Abdallah Mohamed <115114629+AMOHAME0@users.noreply.github.com>
2026-01-27 20:46:56 +0200

    Refactor UserJoinedDate to use getMonth function for month formatting

--- lib/features/profile/presentation/views/widgets/user_joined_date.dart
+++ lib/features/profile/presentation/views/widgets/user_joined_date.dart
@@ -1,20 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:real_time_chat_app/core/functions/get_month.dart';
 import 'package:real_time_chat_app/core/functions/get_user_data.dart';
 import 'package:real_time_chat_app/core/utils/app_theme.dart';
 
 class UserJoinedDate extends StatelessWidget {
-  const UserJoinedDate({
-    super.key,
-  });
+  const UserJoinedDate({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Text(
-      "${getUserData().createdAt.day}-${getUserData().createdAt.month}-${getUserData().createdAt.year}",
-      style: Theme.of(context).textTheme.bodySmall?.copyWith(
-        color: AppTheme.textsecondaryColor,
-      ),
+      "${getUserData().createdAt.day}-${getMonth(month: getUserData().createdAt.month)}-${getUserData().createdAt.year}",
+      style: Theme.of(
+        context,
+      ).textTheme.bodySmall?.copyWith(color: AppTheme.textsecondaryColor),
     );
   }
 }
-
