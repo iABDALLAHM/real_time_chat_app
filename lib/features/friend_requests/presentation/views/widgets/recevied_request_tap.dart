6601592f2b882a2fbdafb0da185b3fd9f1d44306
Abdallah Mohamed <115114629+AMOHAME0@users.noreply.github.com>
2026-02-16 11:51:33 +0200

    refactor SentRequestEmptyStateWidget

--- lib/features/friend_requests/presentation/views/widgets/recevied_request_tap.dart
+++ lib/features/friend_requests/presentation/views/widgets/recevied_request_tap.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:real_time_chat_app/features/friend_requests/presentation/views/widgets/friend_request_item_list_view.dart';
+import 'package:real_time_chat_app/features/friend_requests/presentation/views/widgets/recevied_empty_state_widget.dart';
 
 class ReceviedRequestTap extends StatelessWidget {
   const ReceviedRequestTap({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return FriendRequestItemListView(
-      userEntityList: [],
-      friendRequestEntity: [],
-    );
+    return ReceviedEmptyStateWidget();
   }
 }
--- lib/features/friend_requests/presentation/views/widgets/sent_request_tap.dart
+++ lib/features/friend_requests/presentation/views/widgets/sent_request_tap.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:real_time_chat_app/core/utils/app_theme.dart';
-import 'package:real_time_chat_app/core/widgets/custom_button.dart';
+import 'package:real_time_chat_app/features/friend_requests/presentation/views/widgets/sent_request_empty_state_widget.dart';
 
 class SentRequestTap extends StatelessWidget {
   const SentRequestTap({super.key});
@@ -11,61 +10,3 @@ class SentRequestTap extends StatelessWidget {
   }
 }
 
-class SentRequestEmptyStateWidget extends StatelessWidget {
-  const SentRequestEmptyStateWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Padding(
-        padding: EdgeInsets.all(32),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              width: 80,
-              height: 80,
-              decoration: BoxDecoration(
-                color: AppTheme.primaryColor.withOpacity(0.1),
-                borderRadius: BorderRadius.circular(50),
-              ),
-              child: Icon(
-                Icons.send_outlined,
-                size: 40,
-                color: AppTheme.primaryColor,
-              ),
-            ),
-            const SizedBox(height: 24),
-            Text(
-              "No Sent Requests",
-              style: Theme.of(context).textTheme.headlineSmall?.copyWith(
-                color: AppTheme.textPrimaryColor,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 8),
-            Text(
-              "Friend Requests you send will appear here.",
-              style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                color: AppTheme.textsecondaryColor,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 12),
-            CustomButton(
-              onPressed: () {},
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(Icons.person_search),
-                  const SizedBox(width: 10),
-                  Text("View Friends Requests"),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
