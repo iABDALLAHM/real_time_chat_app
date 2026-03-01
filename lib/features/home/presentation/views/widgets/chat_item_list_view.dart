da36a6806a9485fd7c687d2e807873659abc9c41
Abdallah Mohamed <115114629+AMOHAME0@users.noreply.github.com>
2026-03-01 08:14:10 +0200

    done update unread count

--- lib/features/home/presentation/views/widgets/chat_item_list_view.dart
+++ lib/features/home/presentation/views/widgets/chat_item_list_view.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:real_time_chat_app/core/entities/user_with_chat_entity.dart';
 import 'package:real_time_chat_app/core/functions/get_user_data.dart';
 import 'package:real_time_chat_app/features/chat/presentation/views/chat_view.dart';
+import 'package:real_time_chat_app/features/home/presentation/manager/restore_unreadCount_messages_cubit/restore_unread_count_messages_cubit.dart';
 import 'package:real_time_chat_app/features/home/presentation/views/widgets/chat_item.dart';
 
 class ChatItemListView extends StatelessWidget {
@@ -17,18 +19,7 @@ class ChatItemListView extends StatelessWidget {
           lastMessageTime:
               "${userWithChatList[index].chatEntity.lastMessageTime?.day.toString()}-${userWithChatList[index].chatEntity.lastMessageTime?.month.toString()}-${userWithChatList[index].chatEntity.lastMessageTime?.year.toString()}",
           onTap: () {
-            int myunreadCount =
-                userWithChatList[index].chatEntity.unreadCount[getUserData()
-                    .uId] ??
-                0;
-            int myFriendunreadCount =
-                userWithChatList[index]
-                    .chatEntity
-                    .unreadCount[userWithChatList[index].userEntity.uId] ??
-                0;
-            if (myunreadCount > 0 || myFriendunreadCount > 0) {
-              
-            }
+            handleUpdateUnReadMessagesCount(index: index, context: context);
             Navigator.of(context).pushNamed(
               ChatView.routeName,
               arguments: userWithChatList[index].userEntity,
@@ -38,5 +29,32 @@ class ChatItemListView extends StatelessWidget {
         );
       },
     );
+  }
+
+  void handleUpdateUnReadMessagesCount({
+    required int index,
+    required BuildContext context,
+  }) {
+    var chatId = userWithChatList[index].chatEntity.id;
+
+    int myunreadCount =
+        userWithChatList[index].chatEntity.unreadCount[getUserData().uId] ?? 0;
+    int myFriendunreadCount =
+        userWithChatList[index].chatEntity.unreadCount[userWithChatList[index]
+            .userEntity
+            .uId] ??
+        0;
+
+    if (myunreadCount > 0) {
+      context.read<RestoreUnReadCountMessagesCubit>().restoreunReadCount(
+        userId: getUserData().uId,
+        chatId: chatId,
+      );
+    } else if (myFriendunreadCount > 0) {
+      context.read<RestoreUnReadCountMessagesCubit>().restoreunReadCount(
+        userId: userWithChatList[index].userEntity.uId,
+        chatId: chatId,
+      );
+    }
   }
 }
